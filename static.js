ea94fff9c7ea16f20ffcb76bb336ef51e765e0cf
Erica Xu <iam@ericaxu.com>
2021-02-21 01:25:13 -0500

    Update website

--- static.js
+++ static.js
@@ -1,9 +1,11 @@
 let openHeaderLink = fish('.static-header-menu');
 let headerMobileNav = fish('.static-header-pop-up-nav-container');
 let mobileNavCloseButton = fish('.static-header-pop-up-nav-close-button');
-openHeaderLink.addEventListener('click', () => {
+openHeaderLink.addEventListener('click', (evt) => {
+	evt.preventDefault();
 	headerMobileNav.addClass('is-active');
 });
-mobileNavCloseButton.addEventListener('click', () => {
+mobileNavCloseButton.addEventListener('click', (evt) => {
+	evt.preventDefault();
 	headerMobileNav.removeClass('is-active');
 });
